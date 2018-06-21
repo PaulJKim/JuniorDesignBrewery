91f424d8687981ac17da19c9aacea4431700db20
beyrouti <michael@beyrouti.co>
2018-06-21 13:06:39 -0400

    adding review card changes to branch

---
+++ components/BreweryCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StarRating from 'react-native-star-rating';
+import { Footer, Container, Icon, List, ListItem } from 'native-base';
+
+
+export class BreweryCard extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+	
+		return(
+			title = 
+		);
+	}
+
+
+
+}
